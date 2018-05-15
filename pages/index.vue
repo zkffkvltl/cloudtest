a00f0871d5a5dbcbfef9c3162c674cb98949c25a
zkffkvltl <zkffkvltl@gmail.com>
2018-05-15 10:39:51 +0900

    2018-5-15

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+ <section class="section">
+   <div class="hero is-primary">
+     <div class="hero-body">
+       <div class="container">
+         <h1 class="title is-1">Test</h1>
+         <h2 class="subtitle">Test for cloudserver</h2>
+       </div>
+     </div>
+   </div>
+ </section>
+</template>
+
+<script>
+</script>

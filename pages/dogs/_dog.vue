63f6b80c4037ed25dc5382f396cfecb1a93532a6
zkffkvltl <zkffkvltl@gmail.com>
2018-05-29 11:12:04 +0900

    2018.5.29

--- pages/dogs/_dog.vue
+++ pages/dogs/_dog.vue
@@ -1,6 +1,13 @@
 <template>
     <section class="section">
-{{dogName}}
+        <article class = "hero is-danger">
+            <div class = "hero-body">
+                <div class = "container">
+                    <h1 class="title">{{dogName.toUpperCase()}}</h1>
+                    <h2 class="subtitle">많이 사랑해주세요 :)</h2>
+                </div>
+            </div>
+        </article>
     </section>
 </template>
 <script>
@@ -11,4 +18,4 @@
             };
         }
     }
-</script>
+</script> <!-- _dog의 사용으로 parmas.dog !-->
